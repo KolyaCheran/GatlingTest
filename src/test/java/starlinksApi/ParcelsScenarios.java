4aef35e39821c6455dcbd15b809f3400c6fe3e48
KolyaLoveBublik <nikolay.cheran@gmail.com>
2023-10-30 15:51:41 +0200

    add new scenario create-cancel-track

--- src/test/java/starlinksApi/ParcelsScenarios.java
+++ src/test/java/starlinksApi/ParcelsScenarios.java
@@ -13,17 +13,27 @@ public class ParcelsScenarios {
                     .check(CoreDsl.jmesPath("tracking_number").saveAs("track_number")));
 
     private ChainBuilder cancelParcel = CoreDsl.exec(
-            HttpDsl.http("cancel parcel api call")
-            .post("/cancel")
-            .body(CoreDsl.ElFileBody("bodies/cancelParcel.json")).asJson()
-            .check(HttpDsl.status().is(200),
-                    CoreDsl.substring("\"Canceled\""),
-                    CoreDsl.substring("\"#{track_number}\"")));
+                HttpDsl.http("cancel parcel api call")
+                .post("/cancel")
+                .body(CoreDsl.ElFileBody("bodies/cancelParcel.json")).asJson()
+                .check(HttpDsl.status().is(200),
+                        CoreDsl.substring("\"Canceled\""),
+                        CoreDsl.substring("\"#{track_number}\"")));
+
+    private ChainBuilder trackParcel = CoreDsl.exec(
+                HttpDsl.http("get tracking of parcel")
+                        .post("/history")
+                        .body(CoreDsl.ElFileBody("bodies/getTrackingForParcel.json")).asJson()
+                        .check(CoreDsl.substring("\"#{track_number}\""),
+                                CoreDsl.substring("Parcel created"),
+                                CoreDsl.substring("Parcel canceled")));
 
     private ScenarioBuilder createParcelScenario = CoreDsl.scenario("Create new parcel")
             .exec(createParcel);
     private ScenarioBuilder cancelParcelScenario = CoreDsl.scenario("Cancel parcel")
-            .exec(createParcel).pause(5).exec(cancelParcel) ;
+            .exec(createParcel).pause(2).exec(cancelParcel);
+    private ScenarioBuilder createCancelTrackParcel = CoreDsl.scenario("Create - Cancel - Tracking")
+            .exec(createParcel).pause(2).exec(cancelParcel).pause(2).exec(trackParcel);
 
     public ScenarioBuilder getCreateParcelScenario(){
         return createParcelScenario;
@@ -31,5 +41,9 @@ public class ParcelsScenarios {
 
     public ScenarioBuilder getCancelParcelScenario(){
         return cancelParcelScenario;
+    }
+
+    public ScenarioBuilder getCreateCancelTrackParcel(){
+        return createCancelTrackParcel;
     }
 }
